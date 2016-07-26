d4af9c364d7149322c54c174f4d374637f610927
IAlsu <Alsu_Islamova@epam.com>
2016-07-26 23:03:15 +0600

    Upload Task#2.

---
+++ Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IMe> data = new List<IMe>();
+            InfoData dt = new InfoData();
+            dt.FirstName = "A";
+            dt.LastName = "B";
+
+	        data.Add((IMe)(dt));
+            
+	        Source listOfSourses = new Source();
+	        listOfSourses.CheckAndProceed(data);
+        }
+
+        interface IMe
+        {
+            string FirstName { get; set; }
+            string LastName { get; set; }
+        }
+
+        class InfoData : IMe
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        class Source
+        {
+            internal void CheckAndProceed(List<IMe> data)
+            {
+                var dest = new Destination();
+
+                //do something
+
+                dest.ProceedData(data);
+            }
+        }
+
+        class Destination
+        {
+            internal void ProceedData(List<IMe> data)
+            {
+                foreach (var item in data)
+                {
+                    //do something
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
